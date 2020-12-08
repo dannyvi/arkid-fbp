da3eabfa67be895838ff79fb0d4b08fceb2e7c70
dannyvi <dannyvis@icloud.com>
2020-12-08 11:28:38 +0800

    test push

--- account/flows/admin/nodes/node1.py
+++ account/flows/admin/nodes/node1.py
@@ -5,5 +5,5 @@ from arkfbp.node import FunctionNode
 class Node1(FunctionNode):
 
     def run(self, *args, **kwargs):
-        print(f'Hello, Admin Permission Check Done!')
+        print(f'hi, Hello, Admin Permission Check Done!')
         return True
